5982087f2634d13bb713110cb1ea4a758f9c391e
Felipe Cupitó <65612488+FelipeCupito@users.noreply.github.com>
2024-09-30 15:49:48 -0300

    init

---
+++ src/include/types.h
@@ -0,0 +1,11 @@
+//
+// Created by felu cupito on 30/09/2024.
+//
+
+#ifndef STEGOBMP_TYPES_H
+#define STEGOBMP_TYPES_H
+
+#define SUCCEEDED 1
+#define FAILED    0
+
+#endif //STEGOBMP_TYPES_H
---
+++ src/mian.c
@@ -0,0 +1,3 @@
+//
+// Created by felu cupito on 30/09/2024.
+//
